41c4c8cb0a2b8cfbb7f5e54a649fc9eff89759b1
Ibroximjon Maxammadjonov <ibrokhimjonmakhammadjonov@gmail.com>
2024-06-01 18:53:54 +0500

    java class demo is added

---
+++ class_demo/ClassDemo.java
@@ -0,0 +1,31 @@
+
+
+
+ class Calculator{
+		
+	
+
+	public int add(int num1, int num2){
+	return num1+num2;
+	
+}
+
+
+}
+
+
+
+public class ClassDemo{
+	public static void main(String[] args){
+	
+	Calculator calc = new Calculator();
+	int result = calc.add(3,4);
+	System.out.println(result);
+
+
+
+
+
+	}
+
+}
